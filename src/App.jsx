0557a1426b7558a637bd77e816f504274dadabad
pablinho-programador <pab.rena15@gmail.com>
2025-04-18 11:49:46 -0300

    Criação da função para atualizar o card

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import { useState } from "react"
 export function App() {
 
   const [notaAvaliacao, setNotaAvaliacao] = useState(0)
+  const [rateNote, setRateNote] = useState(0)
+
 
   function handleAlterarNotaAvaliacao(nota){
       setNotaAvaliacao(nota)
@@ -29,11 +31,11 @@ export function App() {
             <p className="text-light-grey text-sm leading-1 mb-6">Please, let us know how we did with your support request. All feedback is appreciated to help us improve our offering"</p>
       
             <div className="flex justify-between mb-6">
-              <input type="button" value={1} className="bg-dark-blue w-10.5 h-10.5 rounded-full text-sm text-medium-grey leading-2 focus:text-dark-blue focus:bg-medium-grey" onClick = {()=> handleAlterarNotaAvaliacao(1)}/>
-              <input type="button" value={2} className="bg-dark-blue w-10.5 h-10.5 rounded-full text-sm text-medium-grey leading-2 focus:text-dark-blue focus:bg-medium-grey" onClick = {()=> handleAlterarNotaAvaliacao(2)}/>
-              <input type="button" value={3} className="bg-dark-blue w-10.5 h-10.5 rounded-full text-sm text-medium-grey leading-2 focus:text-dark-blue focus:bg-medium-grey" onClick = {()=> handleAlterarNotaAvaliacao(3)}/>
-              <input type="button" value={4} className="bg-dark-blue w-10.5 h-10.5 rounded-full text-sm text-medium-grey leading-2 focus:text-dark-blue focus:bg-medium-grey" onClick = {()=> handleAlterarNotaAvaliacao(4)}/>
-              <input type="button" value={5} className="bg-dark-blue w-10.5 h-10.5 rounded-full text-sm text-medium-grey leading-2 focus:text-dark-blue focus:bg-medium-grey" onClick = {()=> handleAlterarNotaAvaliacao(5)}/>
+              <input type="button" value={1} className="bg-dark-blue w-10.5 h-10.5 rounded-full text-sm text-medium-grey leading-2 focus:text-white focus:bg-medium-grey" onClick = {()=> handleAlterarNotaAvaliacao(1)}/>
+              <input type="button" value={2} className="bg-dark-blue w-10.5 h-10.5 rounded-full text-sm text-medium-grey leading-2 focus:text-white focus:bg-medium-grey" onClick = {()=> handleAlterarNotaAvaliacao(2)}/>
+              <input type="button" value={3} className="bg-dark-blue w-10.5 h-10.5 rounded-full text-sm text-medium-grey leading-2 focus:text-white focus:bg-medium-grey" onClick = {()=> handleAlterarNotaAvaliacao(3)}/>
+              <input type="button" value={4} className="bg-dark-blue w-10.5 h-10.5 rounded-full text-sm text-medium-grey leading-2 focus:text-white focus:bg-medium-grey" onClick = {()=> handleAlterarNotaAvaliacao(4)}/>
+              <input type="button" value={5} className="bg-dark-blue w-10.5 h-10.5 rounded-full text-sm text-medium-grey leading-2 focus:text-white focus:bg-medium-grey" onClick = {()=> handleAlterarNotaAvaliacao(5)}/>
             </div>
       
             <button className="bg-orange w-full py-3 uppercase text-sm rounded-3xl tracking-1 font-bold hover:bg-white hover:text-orange" onClick={handeSubmit}>Submit</button>
