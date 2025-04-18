da2d8d7591ac4fd786f03105309ad2722f48cbc2
pablinho-programador <pab.rena15@gmail.com>
2025-04-18 12:12:28 -0300

    Finzalização do segundo card

--- src/App.jsx
+++ src/App.jsx
@@ -45,8 +45,14 @@ export function App() {
             <button className="bg-orange w-full py-3 uppercase text-sm rounded-3xl tracking-1 font-bold hover:bg-white hover:text-orange" onClick={handleSubmit}>Submit</button>
           </div>
       ): (
-        <div className="bg-gradient-dark mx-6 p-6 max-w-103 rounded-2xl text-white font-overpass"> 
-          <img src= {illustrationThankYou} alt="" />
+        <div className="bg-gradient-dark mx-6 p-6 max-w-103 rounded-2xl text-white font-overpass text-center"> 
+          <img className="mx-auto mb-6" src= {illustrationThankYou} alt="Illustration Thank You" />
+
+          <p className="mb-6 text-orange bg-dark-blue w-fit mx-auto px-3 py-1.25 rounded-3xl">You selected {notaAvaliacao} out of 5!</p>
+
+          <h1 className="text-2xl font-bold mb-2.5">Thank you</h1>
+      
+          <p className="text-light-grey text-sm leading-1">We apreciate you taking the time to give a rating. If you ever need more support, don't hesitate to get in touch!"</p>
         </div>
       )}
     </div>
